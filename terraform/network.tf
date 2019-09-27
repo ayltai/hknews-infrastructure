0713caf2a9bc97d50091036c5569e921681057da
Alan Tai <ayltai@gmail.com>
2019-09-27 23:45:48 +0800

    Run terraform fmt

--- terraform/network.tf
+++ terraform/network.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "hknews" {
 resource "aws_eip" "hknews" {
   instance   = aws_instance.hknews[0].id
   vpc        = true
+
   depends_on = [
     "aws_internet_gateway.hknews"
   ]
