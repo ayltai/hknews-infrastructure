64d58406be76276c1a064626be8d210662904e07
Alan Tai <ayltai@gmail.com>
2019-09-28 00:04:24 +0800

    Fix circular reference

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "hknews" {
       sleep 50;
       >inventory.ini;
       echo "[hknews]" | tee -a inventory.ini;
-      echo "${aws_eip.hknews.public_ip} ansible_user=${var.user} ansible_ssh_private_key_file=${var.private_key}" | tee -a inventory.ini;
+      echo "${aws_instance.hknews.public_ip} ansible_user=${var.user} ansible_ssh_private_key_file=${var.private_key}" | tee -a inventory.ini;
       export ANSIBLE_HOST_KEY_CHECKING=False;
       ansible-playbook -u ${var.user} --private-key ${var.private_key} --vault-password-file ${var.vault_password_file} -i inventory.ini ../ansible/playbook.yml
     EOT
