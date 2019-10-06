612b7cc7a49f196be8067a6f710a6e37455fbab6
Alan Tai <ayltai@gmail.com>
2019-10-07 00:29:48 +0800

    Integrate Logz.io

--- ansible/roles/initialize/files/stop.sh
+++ ansible/roles/initialize/files/stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pm2 stop all && pm2 delete all && rm -rf /root/.pm2/logs/* && service mongod stop && rm -rf /var/log/mongodb/mongod.log && service instrumentald stop && rm -rf /var/log/access_log.* /var/log/spring.* /var/log/syslog.* /var/log/auth.log.* /var/log/fail2ban.log /var/log/kern.log.* /var/log/*.1
+pm2 stop all && pm2 delete all && rm -rf /root/.pm2/logs/* && service mongod stop && rm -rf /var/log/mongodb/mongod.log && service instrumentald stop && rm -rf /var/log/syslog.* /var/log/auth.log.* /var/log/fail2ban.log /var/log/kern.log.* /var/log/*.1
